ec18839bcc191a01c8a3a53cd65d50f6f332cfbd
Kakratana <kak.ratana@gmail.com>
2022-04-21 10:27:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function prev(){
       ctx.fillRect(0, 0, 800, 900);
       ctx.fillStyle = "#1876f2";
       ctx.fillRect(0, 0, 800, 100);
-      ctx.fillStyle = "#8bbaf8";
+      ctx.fillStyle = "#f1f3f4";
       ctx.fillRect(0, 750, 800, 50);
       ctx.fillStyle = "#1876f2";
       ctx.fillRect(200, 570, 300, 2);
