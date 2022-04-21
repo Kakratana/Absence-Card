1b55e7b5af370c7724c17ebad563dc08eaaa58e0
Kakratana <kak.ratana@gmail.com>
2022-04-21 14:25:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -42,9 +42,9 @@ function prev(){
       name.innerHTML = inName;
       position.innerHTML = inPosition;
       days.innerHTML =  inDays + " ថ្ងៃ";
-      datef.innerHTML = inDateF + "." + inMonthF + "." + inYearF;
-      datet.innerHTML = inDateT + "." + inMonthT + "." + inYearT;
-      datei.innerHTML = inDateI + "." + inMonthI + "." + inYearI;
+      datef.innerHTML = inDateF + " " + inMonthF + " " + inYearF;
+      datet.innerHTML = inDateT + " " + inMonthT + " " + inYearT;
+      datei.innerHTML = inDateI + " " + inMonthI + " " + inYearI;
       reason.innerHTML = inReason;
       
       
