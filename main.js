d3fdcdf2c918a9389504031c5c942a5e4029d31c
Kakratana <39192742+Kakratana@users.noreply.github.com>
2023-09-01 20:48:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -56,8 +56,8 @@ function prev() {
       reason.innerHTML = inReason;
       time.innerHTML = inTime;
       
-      // ctx.fillStyle = "#00ff00";
-      // ctx.fillRect(0, 0, 800, 900);
+      ctx.fillStyle = "#00ff00";
+      ctx.fillRect(0, 0, 800, 900);
       ctx.fillStyle = "#0D55B5";
       ctx.fillRect(0, 0, 800, 100);
       ctx.fillStyle = "#0D55B5";
